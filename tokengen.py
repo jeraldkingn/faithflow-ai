836966c6aa2a9a031669bc2b434886c6908ff9fe
Jerald King N <jeraldking524.n@gmail.com>
2026-04-10 13:49:08 +0530

    New token gen changes

--- tokengen.py
+++ tokengen.py
@@ -1,6 +1,15 @@
-import base64
+from google_auth_oauthlib.flow import InstalledAppFlow
 
-with open("token.pickle", "rb") as f:
-    print(base64.b64encode(f.read()).decode())
+SCOPES = [
+    "https://www.googleapis.com/auth/youtube.upload",
+    "https://www.googleapis.com/auth/drive.file"
+]
 
+flow = InstalledAppFlow.from_client_secrets_file(
+    "client_secret.json", SCOPES
+)
 
+creds = flow.run_local_server(port=0)
+
+print("ACCESS TOKEN:", creds.token)
+print("REFRESH TOKEN:", creds.refresh_token)
